3f2b1a95f188a24955a3d8c341749b2a4447fa65
YanaulDark <dedark1105@gmail.com>
2026-09-13 10:57:16 +0500

    A few strings code.

--- TestMay/src/main.rs
+++ TestMay/src/main.rs
@@ -7,14 +7,33 @@ fn maybe() -> u8 {
     return 0
 }
 
+struct User {
+    username: String,
+    usermail: String,
+    userpasswd: String,
+    useractive: bool,
+    
+}
 
 
 fn main() {
     println!("---------- Process is running ----------");
-    let mut later = String::new();
+    let mut hellotext = String::from("Hello! Please write your username and passwd");
+    let mut username_text = String::new();
+    let mut passwd = String::new();
+    println!("Username:");
     io::stdin()
-        .read_line(&mut later)
-        .expect("Please write code word");
-    let later = later.trim();
+        .read_line(&mut username_text)
+        .expect("Plase print USER name");
+    println!("Passwd:");
+    io::stdin()
+        .read_line(&mut passwd)
+        .expect("This isn't passwd");
+     let usersname = User {
+        username: String::from(hellotext),
+        userpasswd: String::from(passwd),
+        usermail: String::from("somemail@examle.com"),
+        useractive: true,
+    };
     maybe();
 }
